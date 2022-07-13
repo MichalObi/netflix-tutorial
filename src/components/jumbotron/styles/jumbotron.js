80edc70afb1b1cf11a7145bd9a90c3df8b24d9ec
MichalObi <ndwz123@gmail.com>
2022-07-13 22:24:14 +0200

    Prepare new styled elements - title / subtitle / img

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -15,3 +15,32 @@
   `;
 
 export const Container = styled.div``;
+
+export const Pane = styled.div`
+  width: 50%;
+`;
+
+export const Title = styled.h1`
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
+
+  font-size: 50px;
+  line-height: 1.1;
+  margin-bottom: 8px;
+`;
+
+export const SubTitle = styled.h2`
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+
+  font-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
