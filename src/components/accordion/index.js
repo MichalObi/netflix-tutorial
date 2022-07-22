631d2b14f8aa1501efdabddb5fed481507fff389
MichalObi <ndwz123@gmail.com>
2022-07-22 23:07:51 +0200

    Prepare accordion components

---
+++ src/components/accordion/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>;
+  );
+};
+
+Accordion.Title = function AccordionTitle({children, ...restProps}) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({children, ...restProps}) {
+  return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordion.Item = function AccordionItem({children, ...restProps}) {
+  return <Item {...restProps}>{children}</Item>;
+};
--- src/containers/footer.js
+++ src/containers/footer.js
@@ -35,6 +35,10 @@ export function FooterContainer() {
           <Footer.Link href="#">Legal Notices</Footer.Link>
         </Footer.Column>
       </Footer.Row>
+
+      <Footer.Break />
+
+      <Footer.Text>Netflix United Kingdom</Footer.Text>
     </Footer>
   );
 }
