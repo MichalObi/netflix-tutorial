24b1cae8a3c950b3aca7d753b7a3e472c54f7301
MichalObi <ndwz123@gmail.com>
2022-07-30 21:55:48 +0200

    Fix AccordionHeader to work with image

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -28,7 +28,7 @@ Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
 };
 
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
-  const { toggleShow, setToggleShow } = useState(false);
+  const [toggleShow, setToggleShow] = useState(false);
 
   return (
     <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
@@ -38,17 +38,26 @@ Accordion.Item = function AccordionItem({ children, ...restProps }) {
 };
 
 Accordion.Header = function AccordionHeader({ children, ...restProps }) {
-  const { toggleShow, setToggleShow } = useState(ToggleContext);
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
 
   return (
-    <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
+    <Header onClick={() => setToggleShow(!toggleShow)} {...restProps}>
       {children}
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="Close" />
+      ) : (
+        <img src="/images/icons/add.png" alt="Open" />
+      )}
     </Header>
   );
 };
 
 Accordion.Body = function AccordionBody({ children, ...restProps }) {
-  const { toggleShow } = useState(ToggleContext);
+  const { toggleShow } = useContext(ToggleContext);
 
-  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+  return (
+    <Body className={toggleShow ? 'open' : 'closed'} {...restProps}>
+      <span>{children}</span>
+    </Body>
+  );
 };
