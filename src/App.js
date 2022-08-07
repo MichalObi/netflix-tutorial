fd7c840e06c01742130111ddda401181eb7b39f9
MichalObi <ndwz123@gmail.com>
2022-08-08 00:19:54 +0200

    Prepare route according to react router dom 6.3|

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FaqsContainer } from './containers/faqs';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
+import * as ROUTES from './constants/routes';
+
+console.log('ROUTES x', ROUTES.HOME);
 
 export default function App() {
   return (
-    <>
-     <JumbotronContainer />
-     <FaqsContainer />
-     <FooterContainer />
-    </>
+    <BrowserRouter>
+     <Routes>
+     <Route path={ROUTES.HOME} element=
+     {
+       <>
+         <JumbotronContainer />
+         <FaqsContainer />
+         <FooterContainer />
+       </>
+     } />
+     </Routes>
+    </BrowserRouter>
   );
 }
