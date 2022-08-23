b51d1ca30fcd27596ae073bfde25312d524ebc70
MichalObi <ndwz123@gmail.com>
2022-08-23 22:12:01 +0200

    fix bcg display

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -12,14 +12,6 @@ export function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
-
-      <OptForm>
-        <OptForm.Input placeholder="Email address" />
-        <OptForm.Button>Try it now!</OptForm.Button>
-        <OptForm.Break />
-        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
-      </OptForm>
-
     </Accordion>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { OptForm } from '../components';
 import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
@@ -8,7 +8,15 @@ import { FooterContainer } from '../containers/footer';
 export default function Home() {
   return (
     <>
-      <HeaderContainer />
+      <HeaderContainer>
+        <OptForm>
+          <OptForm.Input placeholder="Email address" />
+          <OptForm.Button>Try it now!</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+        </OptForm>
+      </HeaderContainer>
+
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
