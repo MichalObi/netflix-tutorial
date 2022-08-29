07c324be6c7e25cac92019d3ed93b7162bab9db5
MichalObi <ndwz123@gmail.com>
2022-08-29 23:48:27 +0200

    add sing in form file

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
 
 export default function Signin() {
-  return <p>Hello from the sign in!</p>;
+  return (
+    <>
+      <HeaderContainer>Hello from the sign in!</HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
