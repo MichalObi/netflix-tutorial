706ca7b833828a4b2b90e805d25a4358f1227e0d
MichalObi <ndwz123@gmail.com>
2022-09-01 23:28:59 +0200

    Add form to sign-in component

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
+import { Form } from '../components';
 
 export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState('')
+
   return (
     <>
-      <HeaderContainer>Hello from the sign in!</HeaderContainer>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin}>
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          <Form.Submit disabled={isValid} type="submit">
+            Sign In
+          </Form.Submit>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
