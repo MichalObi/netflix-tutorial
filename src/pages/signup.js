d5c6bf34d6aad2fcb1bd5b95e28330f5bfa8c8b8
MichalObi <ndwz123@gmail.com>
2022-09-10 18:58:28 +0200

    Prepare sign up form

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -22,11 +22,46 @@ export default function Signup() {
   }
 
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign Up</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
-      </Form>
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              placeholder="First Name"
+              value={firstName}
+              onChange={({target}) => setFirstName(target.value)}/>
+
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({target}) => setEmailAddress(target.value)}/>
+
+            <Form.Input
+              type="password"
+              value={[password]}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({target}) => setPassword(target.value)}/>
+
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+
+            <Form.Text>
+              Already a user? <Form.Link to="/signin">Sign in now</Form.Link>
+            </Form.Text>
+
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot. Learn more.
+            </Form.TextSmall>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 }
