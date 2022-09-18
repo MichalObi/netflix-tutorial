81defbfd395e69677bc5dfe6ea42c2920fa2d2f9
MichalObi <ndwz123@gmail.com>
2022-09-18 22:45:06 +0200

    Prepare content use hook

---
+++ src/hooks/use-content.js
@@ -0,0 +1,11 @@
+import { useEffect, useState, useContext } from 'react';
+import { FirebaseContext } from '../context/firebase';
+
+export default function useContent(target) {
+  const [content, setContent] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+
+  }, []);
+}
