235c253961ae0a65999a4dc52525df75b64b99ee
MichalObi <ndwz123@gmail.com>
2022-10-02 22:34:38 +0200

    Prepare profiles styles file

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { selectProfileContainer } from './profiles';
+import { FirebaseContext } from '../context/firebase';
 
 export function BrowseContainer({ slides }) {
-  return <selectProfileContainer />;
+  const [profile, setProfile] = useState({});
+  const {firebase} = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+
+  return <selectProfileContainer
+          user={user} 
+          setProfile={setProfile}/>;
 }
