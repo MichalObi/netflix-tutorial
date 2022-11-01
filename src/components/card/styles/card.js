3de9aee1ae510755820cbe38b51a7ffe3a7ea159
MichalObi <ndwz123@gmail.com>
2022-11-01 23:09:03 +0100

    Prepare card index

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -2,4 +2,10 @@ import styled from 'styled-components/macro';
 
 export const Container = styled.div``;
 
+export const Group = styled.div``;
+
 export const Title = styled.p``;
+
+export const SubTitle = styled.p``;
+
+export const Text = styled.p``;
