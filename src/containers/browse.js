805a2237bac8e1a0a7a47547e805889c9b03ad05
MichalObi <ndwz123@gmail.com>
2022-11-13 23:21:09 +0100

    Prepare Card elements

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -72,6 +72,18 @@ export function BrowseContainer({  }) {
       </Header>
 
       <Card.Group>
+        {setSlideRows.map(slideItem => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map(item => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}/>
+                </Card.Item>
+              ))}
+            </Card.Entities>
+          </Card>
+        ))}
       </Card.Group>
     </>
   ) : (
