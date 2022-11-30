4f7773f3d0996f1ac4ddfa06f657be8cd7ed295f
MichalObi <ndwz123@gmail.com>
2022-11-30 21:45:26 +0100

    Prepare button player styles

---
+++ src/components/player/styles/player.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div``;
+
+export const Overlay = styled.div``;
+
+export const Inner = styled.div``;
+
+export const Close = styled.button``;
+
+export const Button = styled.button`
+  background-color: #e50914;
+  border-color: #ff0a10;
+  width: 114px;
+  height: 45px;
+  text-transform: uppercase;
+  font-weight: bold;
+  color: white;
+  font-size: 18px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: 0;
+`;
