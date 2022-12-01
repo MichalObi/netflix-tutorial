7c741f348ba9ea0a26cd85f867c163b1f6b95c58
MichalObi <ndwz123@gmail.com>
2022-12-01 23:33:18 +0100

    Prepare Overlay styles

--- src/components/player/styles/player.js
+++ src/components/player/styles/player.js
@@ -2,7 +2,18 @@ import styled from 'styled-components/macro';
 
 export const Container = styled.div``;
 
-export const Overlay = styled.div``;
+export const Overlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  margin: 0 20px;
+`;
 
 export const Inner = styled.div``;
 
@@ -22,4 +33,9 @@ export const Button = styled.button`
   align-items: center;
   justify-content: center;
   padding-left: 0;
+
+  &:hover {
+    transform: scale(1.05);
+    background-color: #ff0a16;
+  }
 `;
